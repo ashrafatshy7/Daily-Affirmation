de1dac0d79f0f1c23f7998e3e3c5a17d97be87ae
ashrafatshy7 <ashrafatshy7@gmail.com>
2025-07-08 00:36:51 +0300

    Initial Commit

---
+++ Daily Affirmation/Daily_AffirmationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_AffirmationApp.swift
+//  Daily Affirmation
+//
+//  Created by Ashraf Atshy on 08/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Daily_AffirmationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
